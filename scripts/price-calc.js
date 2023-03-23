4806a679cbcb7e2c214d3632f823535e22b78573
LeoSilvaC <116756375+LeoSilvaC@users.noreply.github.com>
2023-03-23 20:05:19 -0300

    Add files via upload

--- scripts/price-calc.js
+++ scripts/price-calc.js
@@ -1,53 +1,55 @@
 const itens = document.querySelectorAll(".container");
 
 const produtos = {
-    "empada": 4,
-    "acai": 14
-}  
+  empada: 4,
+  acai: 14,
+};
 
-const returnCurrency = (value) => value.toLocaleString('pt-BR',{style:'currency',currency: 'BRL'});
+const returnCurrency = (value) =>
+  value.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 
-itens.forEach(function(element){
-    const botoes = element.querySelectorAll(".controle button")
-
-    botoes.forEach(botao => botao.addEventListener("click", (event) => {
-        atualizaItem(botao.parentElement, event);
-        atualizaTotalItem(element);
-        atualizaTotal();
-    }))
-})
-
-function atualizaItem(controle, botao){
-    const input = controle.querySelector(".controle-contador")
-    const inputValue = parseInt(input.value)
-
-    if(botao.target.innerText === "-" && inputValue > 0){
-        input.value = parseInt(input.value) - 1; 
-    } else if(botao.target.innerText === "+"){
-        input.value = parseInt(input.value) + 1; 
-    }
+export function buttonBehavior(event, container) {
+  atualizaItem(event.target.parentElement, event);
+  atualizaTotalItem(container);
+  atualizaTotal();
 }
 
-function atualizaTotalItem(container){
-    const controles = container.querySelectorAll(".controle");
-    
-    let totalValue = 0;
+function atualizaItem(controle, botao) {
+  const input = controle.querySelector(".controle-contador");
+  const inputValue = parseInt(input.value);
 
-    controles.forEach( (controle) => {
-        totalValue += parseInt(controle.querySelector("input").value) * produtos[controle.dataset.produto];
-    });
-
-    container.querySelector("[data-estatistica]").innerText = returnCurrency(totalValue)
+  if (botao.target.innerText === "-" && inputValue > 0) {
+    input.value = parseInt(input.value) - 1;
+  } else if (botao.target.innerText === "+") {
+    input.value = parseInt(input.value) + 1;
+  }
 }
 
-function atualizaTotal(){
-    let valoresItens = document.querySelectorAll(".container .valor-total");
-    
-    let caixaTotal = 0;
+function atualizaTotalItem(container) {
+  const controles = container.querySelectorAll(".controle");
 
-    valoresItens.forEach(element => {
-        caixaTotal += parseInt(element.innerText.replace(",",".").replace(/[^\d.]/g,""));
-    })
+  let totalValue = 0;
 
-    document.querySelector(".caixa-valor").innerText = returnCurrency(caixaTotal);
+  controles.forEach((controle) => {
+    totalValue +=
+      parseInt(controle.querySelector("input").value) *
+      produtos[controle.dataset.produto];
+  });
+
+  container.querySelector("[data-estatistica]").innerText =
+    returnCurrency(totalValue);
+}
+
+export function atualizaTotal() {
+  let valoresItens = document.querySelectorAll(".container .valor-total");
+
+  let caixaTotal = 0;
+
+  valoresItens.forEach((element) => {
+    caixaTotal += parseInt(
+      element.innerText.replace(",", ".").replace(/[^\d.]/g, "")
+    );
+  });
+
+  document.querySelector(".caixa-valor").innerText = returnCurrency(caixaTotal);
 }
--- scripts/register.js
+++ scripts/register.js
@@ -1,12 +1,33 @@
+import { buttonBehavior, atualizaTotal } from "./price-calc.js";
+
 const register = document.querySelector(".container-registro");
 const botaoAdicionar = register.querySelector(".adc-logo");
 
+function makeid(length) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
 function createContainer(name, produtos) {
   const container = document.createElement("div");
+
   container.classList.add("container");
+
+  function handleButtonBehavior(event) {
+    buttonBehavior(event, container);
+  }
+
   container.innerHTML = `<div class="pessoa">
   <p class="nome-pessoa">${name}</p>
-  <button class="btn-editar">Editar</button>
+  <button class="remove-button">Remover</button>
   <label class="nome">Açaí</label>
   <div class="controle" data-produto="acai">
     <button class="controle-botao">-</button>
@@ -31,7 +52,20 @@ function createContainer(name, produtos) {
   </div>
   <p class="texto-total">Total</p>
   <p class="valor-total" data-estatistica>R$ 0,00</p>
+  
 </div>`;
+
+  const botoes = container.querySelectorAll(".controle-botao");
+  botoes.forEach((botao) => {
+    botao.addEventListener("click", handleButtonBehavior);
+  });
+
+  const botaoRemover = container.querySelector(".remove-button");
+  botaoRemover.addEventListener("click", () => {
+    container.remove();
+    atualizaTotal();
+  });
+
   return container;
 }
 
